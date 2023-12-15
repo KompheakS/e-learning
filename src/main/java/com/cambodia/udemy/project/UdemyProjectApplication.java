bd9b5053e4841f9aef1f5e49143dc9ae8b34dd45
Kompheak <kompheakkh10@gmail.com>
2023-12-15 22:18:11 +0700

    init project

---
+++ src/main/java/com/cambodia/udemy/project/UdemyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cambodia.udemy.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cambodia/udemy/project/UdemyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambodia.udemy.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
