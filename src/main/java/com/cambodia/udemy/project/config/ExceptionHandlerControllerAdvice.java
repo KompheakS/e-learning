3727a09bfba1b4baddf6a9789ec7f963ad762d3d
Kompheak <kompheakkh10@gmail.com>
2024-01-07 15:36:26 +0700

    add all exception handler

--- src/main/java/com/cambodia/udemy/project/config/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/cambodia/udemy/project/config/ExceptionHandlerControllerAdvice.java
@@ -1,7 +1,8 @@
 package com.cambodia.udemy.project.config;
 
-import com.cambodia.udemy.project.dto.CustomErrorResponseException;
 import com.cambodia.udemy.project.exception.CustomBadRequestException;
+import com.cambodia.udemy.project.exception.CustomErrorResponseException;
+import com.cambodia.udemy.project.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -18,13 +19,57 @@ import java.util.Date;
 public class ExceptionHandlerControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomBadRequestException.class)
-    public ResponseEntity<CustomErrorResponseException> handleNotFound(CustomBadRequestException customBadRequestException){
-        CustomErrorResponseException responseException = CustomErrorResponseException.builder()
-                .timestamp(new Date())
+    public ResponseEntity<CustomErrorResponseException<?>> handleNotFound(CustomBadRequestException customBadRequestException){
+        CustomErrorResponseException<Object> responseException = CustomErrorResponseException.builder()
+                .timeStamp(new Date())
                 .status(HttpStatus.BAD_REQUEST.value())
                 .message(customBadRequestException.getMessage())
                 .build();
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseException);
+    }
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<CustomErrorResponseException<?>> handleUnauthorized(GlobalException exception){
+        CustomErrorResponseException<Object> customErrorResponseException = CustomErrorResponseException.builder()
+                .timeStamp(new Date())
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(customErrorResponseException);
+    }
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<CustomErrorResponseException<?>> handleForbidden(GlobalException exception){
+        CustomErrorResponseException<Object> errorResponseException = CustomErrorResponseException.builder()
+                .timeStamp(new Date())
+                .status(HttpStatus.FORBIDDEN.value())
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponseException);
+    }
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<CustomErrorResponseException<?>> handleNotFound(GlobalException exception){
+        CustomErrorResponseException<Object> errorResponseException = CustomErrorResponseException.builder()
+                .timeStamp(new Date())
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponseException);
+    }
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<CustomErrorResponseException<?>> handleInternalServerError(GlobalException exception){
+        CustomErrorResponseException<Object> errorResponseException = CustomErrorResponseException.builder()
+                .timeStamp(new Date())
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponseException);
     }
 }
--- src/main/java/com/cambodia/udemy/project/exception/GlobalException.java
+++ src/main/java/com/cambodia/udemy/project/exception/GlobalException.java
@@ -1,6 +1,6 @@
 package com.cambodia.udemy.project.exception;
 
-public class GlobalException extends Exception{
+public class GlobalException extends RuntimeException{
     private static final long serialVersionUID = -470180507998010368L;
 
     public GlobalException(){
